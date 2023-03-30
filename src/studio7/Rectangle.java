0308350efe4812bb23ece2da5865ca1eb5782114
10370 <10370@TABLET-O13EIHM1.dhcp.wustl.edu>
2023-03-30 12:12:28 -0500

    123

--- src/studio7/Rectangle.java
+++ src/studio7/Rectangle.java
@@ -19,6 +19,20 @@ public class Rectangle {
 		A=length*width;
 		return A;
 	}
+	public boolean compare(double target_area) {
+		double input_A=area();
+		boolean result=input_A<target_area;
+		return result;
+	}
+	public boolean is_square() {
+
+		boolean result;
+		if (length==width)
+			result=true;
+		else
+			result=false;
+		return result;
+	}
 	public double perimeter() {
 		double peri=0;
 		peri=2*(length+width);
@@ -30,9 +44,12 @@ public class Rectangle {
 	public static void main(String[] args) {
 		Rectangle class1=new Rectangle(0.3,0.3);
 		double A=class1.area();
+
 		System.out.println(A);
 		double P=class1.perimeter();
 		System.out.println(P);
 		class1.draw(0.5, 0.5);
+		System.out.println(class1.compare(0.5));
+		System.out.println(class1.is_square());
 	}
 }
